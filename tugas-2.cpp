ead7f8a462f7aaa9e0b8dac3627faac48d6b4a67
Wardana13 <fariwardana@gmail.com>
2024-10-19 13:19:50 +0700

    Add files via upload

---
+++ tugas-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int  nilai;
+cout<<"masukan sebuah nilai";
+cin>>nilai;
+if(nilai>70)
+cout<<"\nlulus";
+else
+cout<<"\nTIDAK LULUS!!!";
+return 0;
+}
